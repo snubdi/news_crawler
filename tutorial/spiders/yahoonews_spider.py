34dc95a3c3b834ebaf5dc636f84535217d39bfbd
snubdi <bigdata2015.snu@gmail.com>
2015-11-17 01:10:48 +0900

    bug fixed--cuishengmin

--- tutorial/spiders/yahoonews_spider.py
+++ tutorial/spiders/yahoonews_spider.py
@@ -17,11 +17,19 @@ class YahoonewsSpider(scrapy.Spider):
     name = 'yahoonews'
     allowed_domains = ['yahoo.co.jp']
     start_urls = ["http://news.yahoo.co.jp/flash?p=1"]
-    
+
+    '''
+    Starting point
+    Retrieve news links from news list pages
+    Args:
+     response - the response object pertaining to the list page
+    '''    
     def parse(self, response):
         try:
+            #get news list
             sites = response.xpath('//*[@id="main"]/div[2]/div/ul/li')
             
+            #get the url of news from the news list
             for site in sites:
                 article = YahoonewsArticleItem()
                 article['title'] = site.xpath('p[1]/a/text()').extract()[0]
@@ -48,7 +56,7 @@ class YahoonewsSpider(scrapy.Spider):
                 news_date = date_1.ljust(11) + hour_minute_1
                 article['date'] = news_date
                 
-                 
+                #determine whether there has video in this news 
                 if site.xpath('p[1]/span/text()'):
                     if u'映像' in site.xpath('p[1]/span/text()').extract()[0]:
                         video = 1
@@ -64,7 +72,8 @@ class YahoonewsSpider(scrapy.Spider):
                 
             pos_2 = response.url.find("=")
             page_count = response.url[pos_2 + 1:]
-
+            
+            #determine whether there has next page
             if int(page_count) < 41:
                 next_url = response.url[:pos_2 + 1] + str(int(page_count) + 1)
                 yield scrapy.Request(next_url, callback = self.parse)
@@ -72,13 +81,21 @@ class YahoonewsSpider(scrapy.Spider):
         except Exception, e:
             print 'ERROR!!!!!!!!!!!!!  URL :'
             print traceback.print_exc(file = sys.stdout)
-            
+    
+    '''
+    Retrieve news links from news list pages
+    Args:
+     response - the response object pertaining to the news page
+     video - 0 or 1 to show whether there has a video in this news
+    '''        
     def parse_news(self, response, video):
         time.sleep(1)
         try:
             
             article = response.meta['article']
             news_url = response.url
+            
+            #determine the location of content based on whether there has a video
             if video == 0:
                 content = response.xpath('//*[@id="main"]/div[1]/div[1]/div[3]/div[1]/p/text()').extract()
                 article['category'] = response.xpath('//div[@class="gnSecWrap"]//li[@class="current"]/a/text()').extract()[0]
@@ -99,7 +116,13 @@ class YahoonewsSpider(scrapy.Spider):
         except Exception, e:
             print 'Parse_news ERROR!!!!!!!!!!!!!  URL :'+ response.url
             print traceback.print_exc(file = sys.stdout)
-    
+            
+    '''
+    Retrieve comment plug in page from comment page
+    Args:
+     response - the response object pertaining to the comment page
+     news_url - url of news page
+    '''
     def get_comment_url(self, response, news_url):
         time.sleep(1)
         try:
@@ -113,15 +136,20 @@ class YahoonewsSpider(scrapy.Spider):
         except Exception, e:
             print 'Get_comment_url ERROR!!!!!!!!!!!!!  URL :'+ response.url
             print traceback.print_exc(file = sys.stdout)
-                
+            
+    '''
+    Retrieve comment of a given news
+    Args:
+     response - the response object pertaining to the page of comment
+    '''
     def parse_comments(self, response):
         time.sleep(1)
         try:
 
             #url_code = urllib.urlopen(response.url).getcode()
-            
+            #get comment list
             sites = response.xpath('//li[@class="commentListItem"]')
-            
+            #get each comment data in the list 
             for site in sites:
                 comment = YahoonewsCommentItem()
                 comment['username'] = site.xpath('div/article/header/h1/a/text()').extract()[0]
@@ -133,7 +161,7 @@ class YahoonewsSpider(scrapy.Spider):
                 pos_1 = response.url.find('/hl?a=')
                 comment['aid'] = response.url[pos_1 + 6:]
                 yield comment
-                
+                #get reply data if have
                 if response.xpath('//li[@class="responseItem repNew"]'):
                     replies = site.xpath('//li[@class="responseItem repNew"]')
                     for reply in replies:
@@ -152,7 +180,7 @@ class YahoonewsSpider(scrapy.Spider):
             pos = response.url.find('page=')
             pos_2 = response.url.find('&keys')
             page_count = response.url[pos + 5: pos_2]
-
+            #determine whether there has next page
             if response.xpath('//*[@id="tabElem0"]/footer//li[@class="next"]/a/@href'):
                 page_count = str(int(page_count) + 1)
                 next_url = response.url[:pos + 5] + page_count + response.url[pos_2:]
