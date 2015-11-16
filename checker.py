3fd1651a0153f68e139121f1de11984af325dd44
snubdi <keith_wzh@hotmail.com>
2015-11-16 18:04:18 +0900

    added yahoonews

--- checker.py
+++ checker.py
@@ -4,24 +4,24 @@ import time
 from datetime import datetime, timedelta
 import scrapy
 import MySQLdb
-import smtplib  
-from email.mime.text import MIMEText  
+import smtplib
+from email.mime.text import MIMEText
 from prettytable import PrettyTable
 
 
 #target medias
-media_list = ['xinhua','163','naver','people','globaltimes','ce']
+media_list = ['xinhua','163','naver','people','globaltimes','ce','yahoonews']
 #DB infomation
 db_host = 'localhost'
 db_name = 'internetNews'
 db_user = 'mers_hwyun'
 db_pw = 'buECAs5ePudeB92R'
 #mailing infomation
-mailto_list=["yunhiwen@hotmail.com","keith_wzh@hotmail.com","cuism1987@hotmail.com","willsong@snu.ac.kr"] 
+mailto_list=["yunhiwen@hotmail.com","keith_wzh@hotmail.com","cuism1987@hotmail.com","willsong@snu.ac.kr"]
 mail_host="smtp.gmail.com"
 mail_user="bigdata2015.snu"
-mail_pass="bigdata2015" 
-mail_postfix="gmail.com"  
+mail_pass="bigdata2015"
+mail_postfix="gmail.com"
 
 #DB connection
 try:
@@ -69,19 +69,18 @@ conn.commit()
 print info
 
 
-
 #send a email
-me="BDIManager"+"<"+mail_user+"@"+mail_postfix+">"  
-msg = MIMEText(info,_subtype='plain',_charset='utf-8')  
+me="BDIManager"+"<"+mail_user+"@"+mail_postfix+">"
+msg = MIMEText(info,_subtype='plain',_charset='utf-8')
 msg['Subject'] = "Crawler daily report " + check_date
-msg['From'] = me  
-msg['To'] = ";".join(mailto_list)  
-try:  
+msg['From'] = me
+msg['To'] = ";".join(mailto_list)
+try:
     server = smtplib.SMTP(mail_host)
     server.ehlo()
     server.starttls()
-    server.login(mail_user,mail_pass)  
-    server.sendmail(me, mailto_list, msg.as_string())  
-    server.close()   
-except Exception, e:  
-    print str(e)  
+    server.login(mail_user,mail_pass)
+    server.sendmail(me, mailto_list, msg.as_string())
+    server.close()
+except Exception, e:
+    print str(e)
